d4497495beab30fe763f7feede2f033d5e7fbff8
mcharnot54 <mmcharnot32@gmail.com>
2025-05-18 01:59:03 -0500

    Add Builder-app public/index.html, src/index.js, CSS and components

---
+++ src/app.js
@@ -0,0 +1,11 @@
+// src/App.js
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Builder + CRA!</h1>
+      {/* …the rest of your app… */}
+    </div>
+  );
+}
