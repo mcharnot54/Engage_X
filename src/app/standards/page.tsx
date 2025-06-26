d3cd0198beb569ba0056d2b1ff8f77eb1024bf6c
mcharnot54 <mmcharnot32@gmail.com>
2025-06-26 11:18:47 -0500

    Move in Observation page

---
+++ src/app/standards/page.tsx
@@ -0,0 +1,7 @@
+// src/app/observation-form/page.tsx
+import BuilderClientPage from "@/components/BuilderClientPage";
+
+export default function Standards() {
+  // slug must match Builder page URL
+  return <BuilderClientPage slug="/standards" />;
+}
